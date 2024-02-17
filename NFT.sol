ab0622bd6f5a2bdaa22b59aba81a63b818084f3f
Utkarsh908 <149078527+Utkarsh908@users.noreply.github.com>
2024-02-17 14:17:17 +0530

    NFT.sol

--- NFT.sol
+++ NFT.sol
@@ -1,2 +1,21 @@
-efnn
-fkjk
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    string private _descriptionPrompt;
+
+    constructor(string memory name, string memory symbol, string memory pDescription) ERC721(name, symbol) Ownable(msg.sender) {
+        _descriptionPrompt = pDescription;
+    }
+
+    function promptDescription() public view returns (string memory) {
+        return _descriptionPrompt;
+    }
+
+    function mint(address to, uint256 tokenId) external onlyOwner {
+        _mint(to, tokenId);
+    }
+}
